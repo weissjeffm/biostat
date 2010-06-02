0f1822dcebc2cf13d3522a8329586fe2e4d61514
Jeff Weiss <jweiss@redhat.com>
2010-06-02 11:10:35 -0400

    privatized some functions, added docstrings

--- src/venn.clj
+++ src/venn.clj
@@ -10,7 +10,7 @@
 	set-labels (take (count list-of-sets) (map str (iterate inc 1)))]
     (map #(with-meta % {:name %2}) list-of-sets set-labels)))
 
-(defn filtered-subsets [set-list]
+(defn- filtered-subsets [set-list]
   (filter #(> (count %) 1) (combinatorics/subsets set-list)))
 
 (defn intersections [sets]
@@ -21,10 +21,10 @@
 		   intr)))
        (filtered-subsets sets)))
 
-(defn left-circular [coll] 
+(defn- left-circular [coll] 
   (concat (rest coll) (list (first coll))))
 
-(defn rotations [coll]
+(defn- rotations [coll]
   (take (count coll) (iterate left-circular coll)))
 
 (defn uniques 
@@ -35,7 +35,8 @@
 	   (concat (list (str "UNIQUES:" (count uniq))) uniq))) 
        (rotations coll)))
 
-(defn venn-diagram [input-filename output-filename]
+(defn venn-diagram "generates data that can be used to construct a venn diagram. Takes an input file (which should be csv formatted, where each row is a set).  Outputs a file listing counts and data for each area of the diagram. There is no hard limit on the number of input sets." 
+  [input-filename output-filename]
   (let [sets (read-sets-from-csv input-filename)]
     (file/spit output-filename (csv/write-csv (uniques sets)))
     (file/append-spit output-filename (csv/write-csv (intersections sets)))))
