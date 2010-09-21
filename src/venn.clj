4eb5a5bde2620c869364eea52176088c417efb24
Jeff Weiss <jweiss@redhat.com>
2010-09-21 16:55:41 -0400

    now handle row headers if necessary, use them as set labels if they exist, otherwise just number the sets

--- src/venn.clj
+++ src/venn.clj
@@ -4,11 +4,17 @@
 	    [com.davidsantiago.csv :as csv]
 	    [clojure.contrib.duck-streams :as file]))
 
-(defn read-sets-from-csv [filename]
-  (let [list-of-sets (map #(disj (set %) "") ;remove empty string from set
-			  (csv/parse-csv (slurp filename)))
-	set-labels (map str (iterate inc 1))]
-    (map #(with-meta % {:name %2}) list-of-sets set-labels))) ;insert the set number as metadata
+(defn read-sets-from-csv
+  ([filename] (read-sets-from-csv filename false))
+  ([filename row-headers]
+     (let [handle-headers (if row-headers rest identity)
+	   rows (csv/parse-csv (slurp filename))
+	   list-of-sets (map #(disj (set (handle-headers %)) "") ;remove column headers if there are any, remove empty string from set
+			     rows)
+	   set-labels (if row-headers
+			(map first rows)
+			(map str (iterate inc 1)))]
+       (map #(with-meta % {:name %2}) list-of-sets set-labels)))) ;insert the set number as metadata
 
 (defn- filtered-subsets [set-list]
   (filter #(> (count %) 1) (combinatorics/subsets set-list))) ;only want subsets where there's more than 1 item to compare
@@ -36,7 +42,7 @@
        (rotations coll)))
 
 (defn venn-diagram "generates data that can be used to construct a venn diagram. Takes an input file (which should be csv formatted, where each row is a set).  Outputs a file listing counts and data for each area of the diagram. There is no hard limit on the number of input sets." 
-  [input-filename output-filename]
-  (let [sets (read-sets-from-csv input-filename)]
+  [input-filename output-filename row-headers]
+  (let [sets (read-sets-from-csv input-filename row-headers)]
     (file/spit output-filename (csv/write-csv (uniques sets)))
     (file/append-spit output-filename (csv/write-csv (intersections sets)))))
