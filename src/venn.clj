ea903821cf4554b1098ccb732a7f32be1bff1120
Jeff Weiss <jweiss@redhat.com>
2010-09-22 14:50:22 -0400

    fixed implementation to match requirements, got rid of unused code

--- src/venn.clj
+++ src/venn.clj
@@ -16,36 +16,20 @@
 			(map str (iterate inc 1)))]
        (map #(with-meta % {:name %2}) list-of-sets set-labels)))) ;insert the set number as metadata
 
-(defn- filtered-subsets [set-list]
-  (filter #(> (count %) 1) (combinatorics/subsets set-list))) ;only want subsets where there's more than 1 item to compare
+(defn calc-venn-piece [sets subset] 
+  (let [intersection (apply set/intersection subset)
+	other-sets (set/difference (set sets) subset)
+	venn-piece (apply set/difference intersection other-sets)]
+    (concat (list (apply str
+			 (interpose "+" (map #(:name (meta %)) subset))))
+	    (list (str "UNIQUE:" (count venn-piece)))
+	    venn-piece)))
 
 (defn venn-pieces [sets]
-  (map (fn [subset] 
-	 (let [intr (apply set/intersection subset)
-	       other-sets (set/difference (set sets)  subset)
-	       venn-piece (apply set/difference intr other-sets)]
-	   (concat (list (apply str
-			    (interpose "+" (map #(:name (meta %)) subset))))
-		   (list (str "INTERSECTION:" (count venn-piece)))
-		   venn-piece)))
-       (filtered-subsets sets)))
-
-(defn- left-circular [coll] 
-  (concat (rest coll) (list (first coll))))
-
-(defn- rotations [coll]
-  (take (count coll) (iterate left-circular coll)))
-
-(defn uniques 
-  "takes a collection of sets, returns a new collection of sets representing the unique items in each set (that don't appear in other sets)" 
-  [coll]
-  (map (fn [rotation] 
-	 (let [uniq (apply set/difference rotation)] 
-	   (concat (list (str "UNIQUES:" (count uniq))) uniq))) 
-       (rotations coll)))
+  (map #(calc-venn-piece sets %) (filter #(> (count %) 0)
+				      (combinatorics/subsets sets))))
 
 (defn venn-diagram "generates data that can be used to construct a venn diagram. Takes an input file (which should be csv formatted, where each row is a set).  Outputs a file listing counts and data for each area of the diagram. There is no hard limit on the number of input sets." 
   [input-filename output-filename row-headers]
   (let [sets (read-sets-from-csv input-filename row-headers)]
-    (file/spit output-filename (csv/write-csv (uniques sets)))
-    (file/append-spit output-filename (csv/write-csv (venn-pieces sets)))))
+    (file/spit output-filename (csv/write-csv (venn-pieces sets)))))
